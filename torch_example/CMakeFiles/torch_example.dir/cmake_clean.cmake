c27385b58ced4eb082773f5adafbe6faf8099bc0
DANIEL VADRANAPU <daniel.vadranapu@fluxauto.xyz>
2021-03-12 07:52:14 +0530

    basic torch example

---
+++ torch_example/CMakeFiles/torch_example.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/torch_example.dir/main.cpp.o"
+  "torch_example"
+  "torch_example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/torch_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
